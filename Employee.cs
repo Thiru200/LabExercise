988937dae1005b37a2f862815c3deca843d05a13
Thiru200 <96133873+Thiru200@users.noreply.github.com>
2024-11-10 22:45:03 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IAM.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? EmailId { get; set; }
+
+    public string? PhoneNo { get; set; }
+
+    public string? Gender { get; set; }
+
+    public bool? Status { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? CreatedDt { get; set; }
+
+    public int? ModifiedBy { get; set; }
+
+    public DateTime? ModifiedDt { get; set; }
+}
